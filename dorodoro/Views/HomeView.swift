d484e2469eaf439564246101d02f56a2c79a1d9d
Derek Hsieh <devderekhsieh@gmail.com>
2021-03-10 23:08:17 -0800

    add home screen basic functions

---
+++ dorodoro/Views/HomeView.swift
@@ -0,0 +1,104 @@
+//
+//  HomeView.swift
+//  dorodoro
+//
+//  Created by Derek Hsieh on 3/10/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    
+    let screen = UIScreen.main.bounds
+    
+    @State private var draggedOffset = CGSize(width: 0, height: UIScreen.main.bounds.height / 1.3)
+    
+    var body: some View {
+        
+        ZStack(alignment: .bottom) {
+       
+            Color(#colorLiteral(red: 0.3490196078, green: 0.6509803922, blue: 0.8862745098, alpha: 1)).edgesIgnoringSafeArea(.all)
+            
+            
+            VStack {
+                
+              
+                    Card()
+                        .offset(y: self.draggedOffset.height)
+                        .gesture(DragGesture()
+                                    .onChanged { value in
+                                        self.draggedOffset = value.translation
+                                    }
+                                    .onEnded { value in
+                                        self.draggedOffset = value.translation
+                                    }
+                        
+                        )
+            }
+            
+            VStack {
+                HStack {
+                    Text("dorodoro")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                        .padding(.leading)
+                    Spacer()
+                }
+                
+                //Play button
+                Spacer()
+             
+                Button(action: {}) {
+                    Image(systemName: "play.fill")
+                        .foregroundColor(.white)
+                        .font(.system(size: 110))
+                        .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 4)
+                                    .frame(width: 150, height: 150)
+                                    .overlay(
+                                        Circle()
+                                            .stroke(Color.white, lineWidth: 4)
+                                            .frame(width: 200, height: 200)
+                                    )
+                                   
+                            )
+                }
+                
+                
+                Spacer()
+                
+                
+              
+                
+            }
+        }
+       
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeView()
+                .previewDevice("iPhone 11")
+            HomeView()
+                .previewDevice("iPhone 12 Pro Max")
+        }
+       
+    }
+}
+
+struct Card: View {
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 26)
+            .fill(Color(#colorLiteral(red: 0.168627451, green: 0.4, blue: 0.662745098, alpha: 1)))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+
